f32445930cdc51ed0436468107be7c2f2fa4d268
donbless27 <donbless0527@gmail.com>
2023-09-11 12:41:57 -0700

    Project 0x0A. Python - Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a class and inherited class-checking function."""
 
+
 def is_kind_of_class(obj, a_class):
     """Returns: True if object is an instance of a class that inherited
      from, the specified class ; otherwise False."""
