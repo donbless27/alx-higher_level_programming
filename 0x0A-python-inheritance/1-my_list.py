7a870a58f0c5a4ef83d74857959eff281ebe2db8
donbless27 <donbless0527@gmail.com>
2023-09-11 08:14:13 -0700

    Project 0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an subclass or child list class MyList."""
+
+class MyList(list):
+    """A subclass of the list class."""
+
+    def print_sorted(self):
+        """Prints the list in ascending sorted order."""
+        print(sorted(self))
