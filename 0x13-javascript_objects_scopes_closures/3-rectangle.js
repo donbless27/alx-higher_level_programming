4ce46ff8cf7b49fa8a9c0ea2eeafd967306d955e
donbless27 <donbless0527@gmail.com>
2023-11-14 08:00:14 -0800

    Project 0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,13 @@
 // A Rectangle class that defines a rectangle
 class Rectangle {
   constructor(w, h) {
-    // Constructor function
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      // Check if the provided parameters are valid
       this.width = w;
       this.height = h;
     }
   }
 
   print() {
-    // Method to print a rectangle
     for (let i = 0; i < this.height; i++) {
       let myVar = '';
       let y = 0;
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,55 +2,36 @@
 
 // Defines a Rectangle class
 class Rectangle {
-  /**
-   * Constructor for the Rectangle class
-   * @param {number} w - Width of the rectangle
-   * @param {number} h - Height of the rectangle
-   */
-  constructor(w, h) {
-    // Check if the provided parameters are valid numbers greater than 0
+  constructor (w, h) {
     if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      // If valid, initialize the instance attributes
       this.width = w;
       this.height = h;
     }
   }
 
-  /**
-   * Print a rectangle made of 'X' characters to the console
-   */
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let myVar = '';
       let y = 0;
-      // Fill each row with 'X' characters (this.width times)
       while (y < this.width) {
         myVar += 'X';
         y++;
       }
 
-      // Print the row to the console
       console.log(myVar);
     }
   }
 
-  /**
-   * Rotate the rectangle by swapping the values of width and height
-   */
-  rotate() {
+  rotate () {
     let temp = 0;
     temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  //Double the dimensions of the rectangle
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
-
-// Export the Rectangle class for use in other modules
 module.exports = Rectangle;
-
