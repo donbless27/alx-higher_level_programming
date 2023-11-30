be7f0c582e70da42baff3ba87732e01f8d0ea641
donbless27 <donbless0527@gmail.com>
2023-11-30 04:54:30 -0800

    Project 0x10 Python Network_0

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that send a DELETE request to an URL with curl
-#    and display the body of the response
+# A script that send a DELETE request to an URL with curl and display the body of the response
 curl -sX DELETE "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,26 +5,29 @@
 def find_peak(list_of_integers):
     """
     Args:
-        list_of_integers(int): list of integers to find peak of
-    Returns: peak of list_of_integers or None
-    """
-    size = len(list_of_integers)
-    mid_e = size
-    mid = size // 2
+    - list_of_integers: List of unsorted integers.
 
-    if size == 0:
+    Returns:
+    - The peak value found in the list.
+    """
+    if not list_of_integers:
         return None
 
-    while True:
-        mid_e = mid_e // 2
-        if (mid < size - 1 and
-                list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid - mid_e // 2
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # If the value at mid is greater than its right neighbor,
+            # a peak might be to the left.
+            high = mid
         else:
-            return list_of_integers[mid]
+            # If the value at mid is less than or equal to its right neighbor,
+            # a peak might be to the right.
+            low = mid + 1
+
+    # At the end of the loop, low and high point to the same element,
+    # which is a peak.
+    return list_of_integers[low]
+
